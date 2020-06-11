acde514b6c8d9366a34bd976539add1256947759
ANS <36783010+answebdev@users.noreply.github.com>
2020-06-10 20:52:42 -0700

    updated code

--- src/components/pages/OpenGraphMetaTags.js
+++ src/components/pages/OpenGraphMetaTags.js
@@ -118,7 +118,7 @@ const OpenGraphMetaTags = () => {
                     'https://coderguides.com/guides/adding-open-graph-meta-tags-to-your-app-or-website', // (defaults to current url)
                   image: 'https://bit.ly/2CMhCMC', // (defaults to og:image or twitter:image)
                   description:
-                    'Guides and notes on various topics related to web development', // (defaults to og:description or twitter:description)
+                    'Adding Open Graph Meta Tags to Your App or Website', // (defaults to og:description or twitter:description)
                   title: 'CoderGuides', // (defaults to og:title or twitter:title)
                   message:
                     'CoderGuides: Adding Open Graph Meta Tags to Your App or Website', // (only for email sharing)
