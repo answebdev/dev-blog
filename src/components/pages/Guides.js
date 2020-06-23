e32c17d98f712165c3566da364a5f1b3f586a27a
ANS <36783010+answebdev@users.noreply.github.com>
2020-06-22 17:23:52 -0700

    edited page

--- src/components/pages/Guides.js
+++ src/components/pages/Guides.js
@@ -49,7 +49,7 @@ const Guides = () => {
                     <p>
                       Add a favicon to your HTML site so that it appears in the
                       browser in the tab section next to the page title.
-                      Continue reading to see how to do this.
+                      Continue reading to see how to.
                     </p>
                   </Card.Text>
                   <Button
