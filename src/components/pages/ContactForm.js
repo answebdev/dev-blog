803d1efe77de007fd58b96d2dbf7935482c292a9
ANS <36783010+answebdev@users.noreply.github.com>
2020-07-18 19:55:38 -0700

    updated contact form text

--- src/components/pages/ContactForm.js
+++ src/components/pages/ContactForm.js
@@ -55,7 +55,7 @@ export class ContactForm extends Component {
                           required
                         />
                         <Form.Text className='text-muted'>
-                          We'll never share your email with anyone else.
+                          Your email will never be shared with anyone else.
                         </Form.Text>
                       </Form.Group>
                       <Form.Group controlId='exampleForm.ControlTextarea1'>
